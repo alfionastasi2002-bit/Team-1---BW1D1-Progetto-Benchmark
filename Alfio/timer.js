9414711fde7a30505ed6661945ed7e89807cfb82
Alfio <alfio.nastasi2002@gmail.com>
2026-05-20 23:08:45 +0200

    creazione js per le domande con altre sistemazioni varie

--- Alfio/timer.js
+++ Alfio/timer.js
@@ -1,16 +1,35 @@
 let secondi = 60;
+let timer; 
+
 const countdown = document.querySelector('#countdown');
-const timerCircle= document.querySelector('.timer-circle');
+const timerCircle = document.querySelector('.timer-circle');
 
-const timer= setInterval(function() {
-    secondi--;
-    countdown.textContent= secondi; 
+function avviaTimer() {
+    clearInterval(timer); 
 
-const percent = (secondi / 60) * 100;
-  timerCircle.style.background = `conic-gradient(#00e5ff ${percent}%, rgba(255,255,255,0.1) 0%)`;
+    secondi = 60;
+    countdown.textContent = secondi;
+    
 
+    timerCircle.style.background = `conic-gradient(#00e5ff 100%, rgba(255,255,255,0.1) 0%)`;
+
+    timer = setInterval(function() {
+        secondi--;
+        countdown.textContent = secondi; 
+        const percent = (secondi / 60) * 100;
+        timerCircle.style.background = `conic-gradient(#00e5ff ${percent}%, rgba(255,255,255,0.1) 0%)`;
 
     if (secondi <= 0) {
         clearInterval(timer);    
-    }
-}, 1000);
+        answersGrid.style.pointerEvents = 'none'; 
+        setTimeout(function() {
+           questionNumber++;
+           nextQuestion();
+           avviaTimer(); 
+           
+           answersGrid.style.pointerEvents = 'auto'; 
+            }, 1000);
+        }
+    }, 1000);
+}
+avviaTimer();
