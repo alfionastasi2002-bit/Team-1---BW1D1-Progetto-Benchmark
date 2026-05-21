6b8cc8900bd2f268b06979da6ddc971acb77a512
Serena-94 <melfaserena@gmail.com>
2026-05-21 23:19:09 +0200

    forse ho risolto

---
+++ Alfio/js/questions.js
@@ -0,0 +1,184 @@
+const questions = [
+    {
+        category: "Science: Computers",
+        type: "multiple",
+        difficulty: "easy",
+        question: "What does CPU stand for?",
+        correct_answer: "Central Processing Unit",
+        incorrect_answers: [
+            "Central Process Unit",
+            "Computer Personal Unit",
+            "Central Processor Unit",
+        ],
+    },
+    {
+        category: "Science: Computers",
+        type: "multiple",
+        difficulty: "easy",
+        question:
+            "In the programming language Java, which of these keywords would you put on a variable to make sure it doesn&#039;t get modified?",
+        correct_answer: "Final",
+        incorrect_answers: ["Static", "Private", "Public"],
+    },
+    {
+        category: "Science: Computers",
+        type: "boolean",
+        difficulty: "easy",
+        question: "The logo for Snapchat is a Bell.",
+        correct_answer: "False",
+        incorrect_answers: ["True"],
+    },
+    {
+        category: "Science: Computers",
+        type: "boolean",
+        difficulty: "easy",
+        question:
+            "Pointers were not used in the original C programming language; they were added later on in C++.",
+        correct_answer: "False",
+        incorrect_answers: ["True"],
+    },
+    {
+        category: "Science: Computers",
+        type: "multiple",
+        difficulty: "easy",
+        question:
+            "What is the most preferred image format used for logos in the Wikimedia database?",
+        correct_answer: ".svg",
+        incorrect_answers: [".png", ".jpeg", ".gif"],
+    },
+    {
+        category: "Science: Computers",
+        type: "multiple",
+        difficulty: "easy",
+        question: "In web design, what does CSS stand for?",
+        correct_answer: "Cascading Style Sheet",
+        incorrect_answers: [
+            "Counter Strike: Source",
+            "Corrective Style Sheet",
+            "Computer Style Sheet",
+        ],
+    },
+    {
+        category: "Science: Computers",
+        type: "multiple",
+        difficulty: "easy",
+        question:
+            "What is the code name for the mobile operating system Android 7.0?",
+        correct_answer: "Nougat",
+        incorrect_answers: [
+            "Ice Cream Sandwich",
+            "Jelly Bean",
+            "Marshmallow",
+        ],
+    },
+    {
+        category: "Science: Computers",
+        type: "multiple",
+        difficulty: "easy",
+        question: "On Twitter, what is the character limit for a Tweet?",
+        correct_answer: "140",
+        incorrect_answers: ["120", "160", "100"],
+    },
+    {
+        category: "Science: Computers",
+        type: "boolean",
+        difficulty: "easy",
+        question: "Linux was first created as an alternative to Windows XP.",
+        correct_answer: "False",
+        incorrect_answers: ["True"],
+    },
+    {
+        category: "Science: Computers",
+        type: "multiple",
+        difficulty: "easy",
+        question:
+            "Which programming language shares its name with an island in Indonesia?",
+        correct_answer: "Java",
+        incorrect_answers: ["Python", "C", "Jakarta"],
+    },
+];
+
+
+
+let questionNumber = 0;
+let score = 0;
+const quizSummary = []
+
+const questionText = document.querySelector('#questionText');
+const answersGrid = document.querySelector('#answersGrid');
+const currentQ = document.querySelector('#currentQ');
+
+function nextQuestion() {
+
+    const domanda = questions[questionNumber]
+    questionText.innerHTML = domanda.question;
+    currentQ.innerHTML = questionNumber + 1;
+    answersGrid.innerHTML = '';
+
+    const risposte = [domanda.correct_answer, ...domanda.incorrect_answers];
+    risposte.sort(() => Math.random() - 0.5); // per avere una probabilità casuale dove andrà a finire la domanda
+
+    risposte.forEach(function (risposta) {
+        const label = document.createElement('label'); // per generare dinamicamente questi elementi nell'html
+        const input = document.createElement('input');
+        const span = document.createElement('span');
+
+        label.className = 'answer'; //assegnazioni classi css
+        input.className = 'radio';
+        span.className = risposta;
+
+        input.type = 'radio'; //configurazione del'input
+        input.name = 'answer';
+        input.value = risposta;
+
+        span.innerHTML = risposta; // il testo dela rispost
+
+        label.onclick = function (event) {
+
+            if (event.target.tagName === 'INPUT') return; // per eliminare il primo input di default
+
+            if (risposta === domanda.correct_answer) {
+                score++;
+
+                quizSummary.push({
+                    questionText: domanda.question,
+                    userAnswer: risposta,
+                    result: "correct"
+                });
+            }else{
+                quizSummary.push({
+                    questionText: domanda.question,
+                    userAnswer: risposta,
+                    result: "wrong"
+                });
+            }
+
+            answersGrid.style.pointerEvents = 'none';
+
+setTimeout(function () {
+                questionNumber++;
+                
+                // CONTROLLO: Ci sono ancora domande?
+                if (questionNumber < questions.length) {
+                    avviaTimer();
+                    nextQuestion();
+                    answersGrid.style.pointerEvents = 'auto';
+                } else {
+                    // SE LE DOMANDE SONO FINITE:
+                    
+                    // 1. Nascondiamo il quiz principale 
+                    document.querySelector('.quiz').style.display = 'none'; 
+                    
+                    // 2. Facciamo partire la TUA funzione che stampa i risultati nel "summary-area"
+                    showFinalSummary();
+                }
+            }, 1000);
+        };
+
+        label.appendChild(input);
+        label.appendChild(span);
+        answersGrid.appendChild(label);
+    });
+}
+
+nextQuestion();
