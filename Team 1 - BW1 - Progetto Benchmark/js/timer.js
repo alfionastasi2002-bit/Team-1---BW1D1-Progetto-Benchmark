ef349cb5ed39b4bd4d3ce99a5b6fde484ac40730
Alfio <alfio.nastasi2002@gmail.com>
2026-05-23 16:34:38 +0200

    Release: Final Project Version

--- Team 1 - BW1 - Progetto Benchmark/js/timer.js
+++ Team 1 - BW1 - Progetto Benchmark/js/timer.js
@@ -1,4 +1,3 @@
-
 let secondi = 60
 let timer
 const countdown = document.querySelector("#countdown")
@@ -7,45 +6,30 @@ const timerCircle = document.querySelector(".timer-circle")
 function avviaTimer() {
   clearInterval(timer)
 
-
   secondi = 60;
 
   countdown.textContent = secondi
 
-
   timerCircle.style.background =
     "conic-gradient(#00e5ff 100%, rgba(255,255,255,0.1) 0%)"
 
-
   timer = setInterval(function () {
-
     secondi--
-
-
     countdown.textContent = secondi
-
-
     const percent = (secondi / 60) * 100
 
-
     timerCircle.style.background = `conic-gradient(#00e5ff ${percent}%, rgba(255,255,255,0.1) 0%)`
-
 
     if (secondi <= 0) {
 
       clearInterval(timer)
 
-
       quizSummary.push({
         questionText: questions[questionNumber].question,
-
         userAnswer: "Nessuna risposta",
-
         correctAnswer: questions[questionNumber].correct_answer,
-
         result: "wrong",
       })
-
 
       vaiAllaProssimaDomanda()
     }
--- Team 1 - BW1 - Progetto Benchmark/js/welcome.js
+++ Team 1 - BW1 - Progetto Benchmark/js/welcome.js
@@ -6,7 +6,6 @@ const checkboxConditions = document.querySelector("#promise-check")
 const proceedButton = document.querySelector(".proceed-btn")
 const errorMessage = document.querySelector("#error")
 
-
 proceedButton.addEventListener("click", function (e) {
   e.preventDefault()
   if (checkboxConditions.checked) {
